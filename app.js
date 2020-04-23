c6ed82ac8691447ff05ebf2ed7a8626f81fff93b
VladislavOnishchenko <vladionishchenko@gmail.com>
2020-04-23 10:19:39 +0300

    Added 404 page

--- app.js
+++ app.js
@@ -12,5 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 Page was not found</h1>')
+});
+
 app.listen(3000);
 
