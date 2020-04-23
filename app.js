8f7c07d86fe99d3614ba7ded1f255003a93cad0a
VladislavOnishchenko <vladionishchenko@gmail.com>
2020-04-23 09:37:25 +0300

    Routes added

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+
+app.use('/', (req, res, next) => {
+    console.log('Guard');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Add Product</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
+
